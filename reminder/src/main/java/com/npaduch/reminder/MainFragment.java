ec0198ca51b9e49c049486b3778a4dd1c0ee9d21
nolanpaduch <n.paduch1@gmail.com>
2014-05-06 22:31:16 -0400

    Created reminder card

--- reminder/src/main/java/com/npaduch/reminder/MainFragment.java
+++ reminder/src/main/java/com/npaduch/reminder/MainFragment.java
@@ -5,9 +5,12 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by nolanpaduch on 5/3/14.
+ *
  */
 
 public class MainFragment extends Fragment {
@@ -19,6 +22,25 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        initializeSpinners(rootView);
         return rootView;
+    }
+
+
+    public  void initializeSpinners(View view){
+
+        // locate spinners
+        Spinner daySpinner = (Spinner) view.findViewById(R.id.newNoteDaySpinner);
+        Spinner timeSpinner = (Spinner) view.findViewById(R.id.newNoteTimeSpinner);
+
+        // setup day spin adapter
+        ArrayAdapter<CharSequence> dayAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.spinner_day, android.R.layout.simple_spinner_item);
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daySpinner.setAdapter(dayAdapter);
+
+        // setup time spin adapter
+        ArrayAdapter<CharSequence> timeAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.spinner_time, android.R.layout.simple_spinner_item);
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(timeAdapter);
     }
 }
