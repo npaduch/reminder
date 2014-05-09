a1e9b9dc26c6fa363a64b56d945627734c04d3ff
nolanpaduch <n.paduch1@gmail.com>
2014-05-08 22:41:34 -0400

    New reminder action bar

--- reminder/src/main/java/com/npaduch/reminder/NewReminderFragment.java
+++ reminder/src/main/java/com/npaduch/reminder/NewReminderFragment.java
@@ -3,6 +3,8 @@ package com.npaduch.reminder;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,11 +23,19 @@ public class NewReminderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.new_reminder, container, false);
-        //initializeSpinners(rootView);
 
         ReminderCardView myCard = new ReminderCardView(getActivity());
         myCard.description.setText("This is another description, purely to avoid warnings...");
 
+        setHasOptionsMenu(true);
+
         return rootView;
     }
+
+    @Override
+    public void onCreateOptionsMenu(
+            Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.new_reminder, menu);
+    }
+
 }
