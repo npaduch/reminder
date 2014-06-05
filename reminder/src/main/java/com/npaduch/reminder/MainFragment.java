4d27e9264f9812172ca66d340b42a60aeb0ede9d
nolanpaduch <n.paduch1@gmail.com>
2014-06-05 00:47:42 -0400

    Clean up unused imports.

--- reminder/src/main/java/com/npaduch/reminder/MainFragment.java
+++ reminder/src/main/java/com/npaduch/reminder/MainFragment.java
@@ -3,7 +3,6 @@ package com.npaduch.reminder;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
