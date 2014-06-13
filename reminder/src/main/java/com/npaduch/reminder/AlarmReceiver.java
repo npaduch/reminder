304f03688c3835e68d3f78c931f86f1c7169f789
nolanpaduch <n.paduch1@gmail.com>
2014-06-12 22:59:07 -0400

    Added notification tone, LED, and vibrate.

--- reminder/src/main/java/com/npaduch/reminder/AlarmReceiver.java
+++ reminder/src/main/java/com/npaduch/reminder/AlarmReceiver.java
@@ -1,14 +1,15 @@
 package com.npaduch.reminder;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Created by nolanpaduch on 6/11/14.
@@ -24,11 +25,16 @@ public class AlarmReceiver extends BroadcastReceiver{
     // Random ID for notification
     private static final int mId = 10492842;
 
+    // length to flash LED  in ms
+    private static final int LED_ON_TIME = 3000;
+    private static final int LED_OFF_TIME = 3000;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "Reminder Due.");
         throwNotification(context);
 
+        //vibrateDevice(context);
     }
 
     public void throwNotification(Context context){
@@ -59,9 +65,18 @@ public class AlarmReceiver extends BroadcastReceiver{
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
         mBuilder.setContentIntent(resultPendingIntent);
+
+        // set default alert tones/lights/vibrations
+        mBuilder.setLights(Color.GREEN, LED_ON_TIME, LED_OFF_TIME);
+        Notification note = mBuilder.build();
+        note.defaults |= Notification.DEFAULT_VIBRATE;
+        note.defaults |= Notification.DEFAULT_SOUND;
+
         NotificationManager mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         // mId allows you to update the notification later on.
-        mNotificationManager.notify(mId, mBuilder.build());
+        mNotificationManager.notify(mId, note);
+
     }
+
 }
