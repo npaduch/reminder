724e2236aadfdbb57b17d3bd5ba6c76c96a9bd24
nolanpaduch <n.paduch1@gmail.com>
2014-06-20 21:51:40 -0400

    Cleanup 2

--- reminder/src/main/java/com/npaduch/reminder/MainFragment.java
+++ reminder/src/main/java/com/npaduch/reminder/MainFragment.java
@@ -117,7 +117,8 @@ public class MainFragment extends Fragment {
     }
 
 
-    // TODO:
+    // This is different from the reminder class's instance!
+    // It will create a new file if one is not found.
     private ArrayList<Reminder> getJSONFileContents(){
         Log.d(TAG, "Looking for file " + getActivity().getFilesDir() + File.pathSeparator + Reminder.filename);
 
