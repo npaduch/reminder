b264a5d54be58ce674c5bf9965b6e7aac7fd8290
nolanpaduch <n.paduch1@gmail.com>
2014-08-17 23:32:16 -0400

    Settings outined reworked.

--- reminder/src/main/java/com/npaduch/reminder/MainSettingsFragment.java
+++ reminder/src/main/java/com/npaduch/reminder/MainSettingsFragment.java
@@ -1,22 +1,76 @@
 package com.npaduch.reminder;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.view.CardView;
 
 /**
  * Created by nolanpaduch on 8/14/14.
  *
  * Fragment to display the main settings for the app
  */
-//public class MainSettingsFragment extends PreferenceFragment {
-public class MainSettingsFragment extends PreferenceFragment {
+
+public class MainSettingsFragment extends Fragment {
+
+    private final static String TAG = "MainSettings";
+
+    // View for fragment
+    private View rootView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.main_preferences);
+        Log.d(TAG, "New Reminder OnCreateView");
+
+        // inflate view first
+        rootView = inflater.inflate(R.layout.preference_layout, container, false);
+
+
+        // Create a Card
+        Card card = new Card(getActivity(), R.layout.card_time_preference);
+
+        // Set card in the cardView
+        CardView cardView = (CardView) rootView.findViewById(R.id.preferenceTimeLayout);
+        cardView.setCard(card);
+
+        initDefaultTimes(getActivity(), rootView);
+
+        return rootView;
     }
 
+    private void initDefaultTimes(Context context, View rootview){
+        TextView morningValue = (TextView)rootView.findViewById(R.id.timePreferenceValueMorning);
+        TextView noonValue = (TextView)rootView.findViewById(R.id.timePreferenceValueNoon);
+        TextView afternoonValue = (TextView)rootView.findViewById(R.id.timePreferenceValueAfternoon);
+        TextView eveningValue = (TextView)rootView.findViewById(R.id.timePreferenceValueEvening);
+        TextView nightValue = (TextView)rootView.findViewById(R.id.timePreferenceValueNight);
+
+        Calendar cal = Calendar.getInstance();
+        SettingsHandler settingsHandler = new SettingsHandler();
+
+        cal.setTimeInMillis(settingsHandler.getTimeMorning(context));
+        morningValue.setText(Reminder.buildTimeString(context, cal));
+        cal.setTimeInMillis(settingsHandler.getTimeNoon(context));
+        noonValue.setText(Reminder.buildTimeString(context, cal));
+        cal.setTimeInMillis(settingsHandler.getTimeAfternoon(context));
+        afternoonValue.setText(Reminder.buildTimeString(context, cal));
+        cal.setTimeInMillis(settingsHandler.getTimeEvening(context));
+        eveningValue.setText(Reminder.buildTimeString(context, cal));
+        cal.setTimeInMillis(settingsHandler.getTimeNight(context));
+        nightValue.setText(Reminder.buildTimeString(context, cal));
+    }
+
+
 }
+
--- reminder/src/main/java/com/npaduch/reminder/SettingsHandler.java
+++ reminder/src/main/java/com/npaduch/reminder/SettingsHandler.java
@@ -15,7 +15,20 @@ public class SettingsHandler {
     private final static String TAG = "SettingsHandler";
 
     private final static String nextId = "Next_ID";
+    private final static String timeMorning = "time_morning";
+    private final static String timeNoon = "time_noon";
+    private final static String timeAfternoon = "time_afternoon";
+    private final static String timeEvening = "time_evening";
+    private final static String timeNight = "time_night";
+
+    private final static long timeDefaultMorning = 1000 * 60 * 60 * 9;
+    private final static long timeDefaultNoon = 1000 * 60 * 60 * 12;
+    private final static long timeDefaultAfternoon = 1000 * 60 * 60 * (12 + 3);
+    private final static long timeDefaultEvening = 1000 * 60 * 60 * (12 + 5);
+    private final static long timeDefaultNight = 1000 * 60 * 60 * (12 + 8);
+
     private final static int badId = -1;
+    private final static int badTime = -1;
 
     public SettingsHandler(){};
 
@@ -40,6 +53,66 @@ public class SettingsHandler {
         editor.commit();
 
         return currentID;
+    }
+
+    public long getTimeMorning(Context context){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sp.edit();
+
+        // handle case where ID has never been set
+        if(!sp.contains(timeMorning)){
+            editor.putLong(timeMorning, timeDefaultMorning); // value to store
+            editor.commit();
+        }
+        return sp.getLong(timeMorning, badTime);
+    }
+
+    public long getTimeNoon(Context context){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sp.edit();
+
+        // handle case where ID has never been set
+        if(!sp.contains(timeNoon)){
+            editor.putLong(timeNoon, timeDefaultNoon); // value to store
+            editor.commit();
+        }
+        return sp.getLong(timeNoon, badTime);
+    }
+
+    public long getTimeAfternoon(Context context){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sp.edit();
+
+        // handle case where ID has never been set
+        if(!sp.contains(timeAfternoon)){
+            editor.putLong(timeAfternoon, timeDefaultAfternoon); // value to store
+            editor.commit();
+        }
+        return sp.getLong(timeAfternoon, badTime);
+    }
+
+    public long getTimeEvening(Context context){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sp.edit();
+
+        // handle case where ID has never been set
+        if(!sp.contains(timeEvening)){
+            editor.putLong(timeEvening, timeDefaultEvening); // value to store
+            editor.commit();
+        }
+        return sp.getLong(timeEvening, badTime);
+    }
+
+    public long getTimeNight(Context context){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sp.edit();
+
+        // handle case where ID has never been set
+        if(!sp.contains(timeNight)){
+            editor.putLong(timeNight, timeDefaultNight); // value to store
+            editor.commit();
+        }
+        return sp.getLong(timeNight, badTime);
     }
 
 }
